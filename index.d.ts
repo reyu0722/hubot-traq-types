b11f0dfa22fceacb78a23d9e382c2e593fd6776a
reyu <ka15sugar@gmail.com>
2021-03-05 17:01:05 +0900

    init

---
+++ index.d.ts
@@ -0,0 +1,42 @@
+import { Response as _Response, Robot as _Robot } from 'hubot'
+
+declare namespace HubotTraq {
+  class Response extends _Response {
+    message: {
+      eventTime: string
+      message: {
+        id: string
+        user: {
+          id: string
+          name: string
+          displayName: string
+          iconId: string
+          bot: boolean
+        }
+        channelId: string
+        text: string
+        plainText: string
+        embedded: [
+          {
+            raw: string
+            type: string
+            id: string
+          }
+        ]
+        createdAt: string
+        updatedAt: string
+      }
+      user: any
+      id: string
+      text: string
+      room: any
+      finish(): void
+    }
+  }
+  class Robot extends _Robot {
+    respond(regex: RegExp, callback: (response: Response) => void): void
+  }
+}
+
+export = HubotTraq
+export as namespace HubotTraq
